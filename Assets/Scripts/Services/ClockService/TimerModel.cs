34d4000eedd14dda249edd74ce0bd1b307460ea8
Antonio Narvaez <antoniojesusnc@gmail.com>
2026-01-30 20:31:09 +0100

    rythm manager

---
+++ Assets/Scripts/Services/ClockService/TimerModel.cs
@@ -0,0 +1,69 @@
+using System;
+using UnityEngine;
+
+namespace ggj26.Services
+{
+    [Serializable]
+    public class TimerModel : IDisposable
+    {
+        private static int TIMER_MODEL_ID = 0;
+
+        public bool IsPausable { get; private set; } = true;
+        public float Id { get; private set; }
+        [field: SerializeField]
+        public float Duration { get; private set; }
+        [field: SerializeField]
+        public float RemainingTime { get; private set; }
+        public Action FinishCallback { get; private set; }
+
+        public bool IsInCooldown => RemainingTime > 0;
+        public bool HasCooldown => Duration > 0;
+        public bool Disposed { get; private set; }
+        
+        public TimerModel(float duration) : this(duration, null, true) { }
+        public TimerModel(float duration, Action finishCallback) : this(duration, finishCallback, true) { }
+
+        public TimerModel(float duration, Action finishCallback, bool isPausable)
+        {
+            Id = TIMER_MODEL_ID++;
+            Duration = duration;
+            FinishCallback = finishCallback;
+            IsPausable = isPausable;
+        }
+        
+        public void SetDuration(float newDuration)
+        {
+            Duration = newDuration;
+        }
+
+        private void CallFinishCallback()
+        {
+            FinishCallback?.Invoke();
+        }
+
+        public void BeginTimer(Action finishCallback)
+        {
+            RemainingTime = Duration;
+            FinishCallback = finishCallback;
+        }
+
+        public void DeductTime(float deltaTime)
+        {
+            RemainingTime -= deltaTime;
+            if (RemainingTime <= 0)
+            {
+                CallFinishCallback();
+            }
+        }
+
+        public void ForceFinish()
+        {
+            DeductTime(RemainingTime);
+        }
+
+        public void Dispose()
+        {
+            Disposed = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/DontDestroyOnLoad.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace ggj26
+{
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
