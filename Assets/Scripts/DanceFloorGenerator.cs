9f53ab24178499dbe5313204716d32beaa1b792e
Jordi Garcia <matazombis@gmail.com>
2026-01-30 23:47:55 +0100

    Implementada pista de baile y zona de UI para las barras de ritmo.

--- Assets/Scripts/DanceFloorGenerator.cs
+++ Assets/Scripts/DanceFloorGenerator.cs
@@ -1,16 +1,65 @@
+using ggj26;
+using ggj26.Event;
+using Supyrb;
 using UnityEngine;
 
 public class DanceFloorGenerator : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject danceTile;
+    [SerializeField] private int danceFloorWidth = 6;
+    [SerializeField] private int danceFloorHeight = 4;
+    [SerializeField] private float tileSeparation = 1.0f;
+
+    private GameObject[][] danceTiles;
+    private int currentColor = 0;
+
+    private void Start()
     {
-        
+        danceTiles = new GameObject[danceFloorWidth][];
+        for (int i = 0; i < danceFloorWidth; i++)
+        {
+            danceTiles[i] = new GameObject[danceFloorHeight];
+            for(int j = 0; j < danceFloorHeight; j++)
+            {
+                danceTiles[i][j] = Instantiate(danceTile, transform);
+                danceTiles[i][j].transform.position = transform.position + new Vector3(i, j, 0) * tileSeparation - new Vector3 (danceFloorWidth*tileSeparation/2, danceFloorHeight * tileSeparation / 2, 0);
+            }
+        }
+
+        Signals.Get<OnBeatEvent>().AddListener(ChangeFloorColors);
+
+        ChangeFloorColors();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeFloorColors();
+        }
     }
+
+    private void ChangeFloorColors()
+    {
+        Color color1 = RhythmManager.Instance.Config.Colors[currentColor];
+        Color color2 = RhythmManager.Instance.Config.Colors[currentColor + 1 == RhythmManager.Instance.Config.Colors.Length ? 0 : currentColor + 1];
+
+        for (int i = 0; i<danceFloorWidth; i++)
+        {
+            for (int j = 0; j<danceFloorHeight; j++)
+            {
+                if ((i+j)%2==0)
+                {
+                    danceTiles[i][j].GetComponent<SpriteRenderer>().color = color1;  
+                }
+                else 
+                {
+                    danceTiles[i][j].GetComponent<SpriteRenderer>().color = color2;
+                }
+            }
+        }
+
+        currentColor = currentColor + 1 == RhythmManager.Instance.Config.Colors.Length ? 0 : currentColor + 1;
+    }
+
 }
